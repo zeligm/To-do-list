6e923a8bba7f53c4eb319feedd1dd45f3f72b988
Zelig Malamud <zeligmalamud@gmail.com>
2018-06-05 20:53:29 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //Problem: User interaction doesn't provide desired results.
-//Solution: Add interactivity so the user can manage daily tasks.
+//Solution: Add interactivity so the user can manage daily tasks. 
 
 var taskInput = document.getElementById('new-task'); //#new-task
 var addButton = document.getElementsByTagName('button')[0]; //first button
